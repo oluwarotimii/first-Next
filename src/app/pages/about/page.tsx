ff0ce5c2070113828ab7beeb587881af732ba44a
oluwarotimii <oluwarotimiadewumi@gmail.com>
2023-11-02 10:23:17 +0100

    commit

---
+++ src/app/pages/about/page.tsx
@@ -0,0 +1,14 @@
+// pages.tsx
+
+import Link from 'next/link'
+
+function index() {
+  return (
+    <div>
+      <h1>Hello, Next.js!</h1>
+      <p> This is the  about page</p>
+    </div>
+  );
+}
+
+export default index;
---
+++ src/app/pages/landing/page.tsx
@@ -0,0 +1,14 @@
+// pages.tsx
+
+import Link from 'next/link'
+
+function landingPage() {
+  return (
+    <div>
+      <h1>Hello, Next.js!</h1>
+      <p> This is the  Landing Page</p>
+    </div>
+  );
+}
+
+export default landingPage;
